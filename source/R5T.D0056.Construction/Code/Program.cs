dc85776808712fd9f63647d86d06e984721c6342
David Coats <david@getsafetycone.com>
2021-05-05 18:03:28 -0700

    Add initial source stub.

---
+++ source/R5T.D0056.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.D0056.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
